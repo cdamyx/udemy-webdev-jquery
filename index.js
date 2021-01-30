17e66a22a6cbbbab92dee83c45b66eb544293223
cdamyx <chris@amyx.engineering>
2021-01-29 22:32:18 -0600

    lesson 182 event listeners with jQuery complete

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 $("h1").addClass("big-title margin-50");
 
-$("button").css()
+$("button").html("<em>hey</em>");
+
+$("h1").click(function() {
+	$("h1").css("color", "purple");
+})
+
+$("button").click(function() {
+	$("h1").css("color", "purple");
+})
+
+$(document).keypress(function(event) {
+	$("h1").text(event.key);
+})
+
+$("h1").on("mouseover", function() {
+	$("h1").css("color", "purple");
+})
